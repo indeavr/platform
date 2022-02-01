dc604dcc5a62495ace2f7716be0d8fbd255fbe2f
Devon Weller <dweller@devonweller.com>
2022-01-31 21:05:02 -0600

    Temporary: Example on community page

--- frontend/src/pages/Community.tsx
+++ frontend/src/pages/Community.tsx
@@ -1,25 +1,62 @@
 import { useState } from 'react';
-import { Modal } from 'components/ui/Modal';
+import { useContracts } from 'components/blockchain-contracts/useContracts';
 
 const Community = () => {
-  const [showModal, setShowModal] = useState(true);
+  const [ownerAddress, setOwnerAddress] = useState<string | undefined>(
+    undefined
+  );
+  const contracts = useContracts();
+
+  const getContractOwner = async () => {
+    if (!contracts.Job) {
+      return;
+    }
+    const owner = await contracts.Job.owner();
+    setOwnerAddress(owner);
+  };
 
   // find Icon types at https://unpkg.com/browse/@heroicons/react@1.0.5/outline/index.js
   return (
     <>
       <div>Community</div>
 
-      <Modal
-        title="This Page Isn't Ready Yet"
-        icon="InformationCircleIcon"
-        closeButton="Got it"
-        isOpen={showModal}
-        onRequestClose={() => {
-          setShowModal(false);
-        }}
-      >
-        <p>We're working on this page. It isn't ready yet.</p>
-      </Modal>
+      <div className="mt-5">
+        <pre>owner: {ownerAddress}</pre>
+        <div className="mt-2">
+          <button
+            onClick={getContractOwner}
+            type="button"
+            className="focus:outline-none inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Get Contract Owner
+          </button>
+        </div>
+      </div>
+
+      <div className="mt-5">
+        Is ERC 20 token approved?
+        <pre>{contracts.isERC20Approved ? 'YES' : 'NO'}</pre>
+      </div>
+      <div className="mt-2">
+        {!contracts.isERC20Approved && (
+          <button
+            onClick={contracts.approveERC20}
+            type="button"
+            className="focus:outline-none inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Approve
+          </button>
+        )}
+        {contracts.isERC20Approved && (
+          <button
+            onClick={contracts.revokeERC20}
+            type="button"
+            className="focus:outline-none inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-white shadow-sm hover:bg-red-700 focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+          >
+            Revoke
+          </button>
+        )}
+      </div>
     </>
   );
 };
